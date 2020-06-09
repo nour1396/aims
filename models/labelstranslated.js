61f26be718df6645ac0d1922b572635056aa66d5
nourhanElkashef <nourhan@roots.solutions>
2020-06-09 13:04:37 +0200

    no message

--- models/labelstranslated.js
+++ models/labelstranslated.js
@@ -1,17 +1,10 @@
 const mongoose = require("mongoose");
 var labelsTranslateSchema = new mongoose.Schema({
-    english1: String,
-    english2: String,
-    english3: String,
-    english4: String,
-    english5: String,
-    english6: String,
-    spanish1: String,
-    spanish2: String,
-    spanish3: String,
-    spanish4: String,
-    spanish5: String,
-    spanish6: String,
+    _id: String,
+    english: String,
+    arabic: String,
+    spanish: String,
+    Farsi: String
 })
 const labelstranslated = mongoose.model("newlabels", labelsTranslateSchema);
 module.exports = labelstranslated;
